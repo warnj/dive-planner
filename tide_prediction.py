f1e4124ea3940302138d9a011e30087a3f0bbecd
Justin Warner <jkwdives@hotmail.com>
2023-08-02 15:31:13 -0700

    Add Sekiu with offset for outer:

--- tide_prediction.py
+++ tide_prediction.py
@@ -60,14 +60,16 @@ def main():
 
     # ---------------------------------- MANUALLY CONFIGURABLE PARAMETERS ---------------------------------------------
     STATIONS = []
-    # STATIONS.append('Ayock Point')
     # STATIONS.append('Bowman Bay')
     # STATIONS.append('Hanbury Point, North San Juan Island')
     # STATIONS.append('Kanaka Bay, South San Juan Island')
-    STATIONS.append('La Push')
-    args.START = dt(2023, 6, 17)
+    STATIONS.append('Sekiu')
+    STATIONS.append('Crescent Bay')
+    # STATIONS.append('Ayock Point')
+    # STATIONS.append('La Push')
+    args.START = dt(2023, 7, 29)
     # args.START = dt.now()
-    args.DAYS_IN_FUTURE = 2
+    args.DAYS_IN_FUTURE = 1
     args.INCLUDE_WORKDAYS = True
     # ------------------------------------------------------------------------------------------------------------------
 
